fcc97a717265b0190d358e1e14e086406d964259
Ariel Mashraki <ariel@mashraki.co.il>
2015-11-15 09:04:39 +0200

    feat(tree): add file size

--- info.go
+++ info.go
@@ -65,12 +65,33 @@ func (inf *info) print(indent string, opts *options) {
 		fmt.Printf("%s [%s]\n", inf.path, err)
 		return
 	}
-	// Print name/path
+	var name, size string
+	// name/path
 	if inf.depth == 0 || opts.fullPath {
-		fmt.Println(inf.path)
+		name = inf.path
 	} else {
-		fmt.Println(inf.Name())
+		name = inf.Name()
 	}
+	if !inf.IsDir() {
+		if opts.byteSize {
+			size = fmt.Sprintf("%d", inf.Size())
+			if pad := 11 - len(size); pad > 0 {
+				size = strings.Repeat(" ", pad) + size
+			}
+		}
+		if opts.unitSize {
+			size = formatBytes(inf.Size())
+			if pad := 4 - len(size); pad > 0 {
+				size = strings.Repeat(" ", pad) + size
+			}
+		}
+		// Print properties
+		if props := size; len(props) > 0 {
+			fmt.Printf("[%s]  ", props)
+		}
+	}
+	// Print file details
+	fmt.Println(name)
 	add := "│   "
 	for i, _inf := range inf.infos {
 		if i == len(inf.infos)-1 {
@@ -81,4 +102,30 @@ func (inf *info) print(indent string, opts *options) {
 		}
 		_inf.print(indent+add, opts)
 	}
+}
+
+// Convert bytes to human readable string. Like a 2 MB, 64.2 KB, 52 B
+func formatBytes(i int64) (result string) {
+	var n float64
+	sFmt, eFmt := "%.01f", ""
+	switch {
+	case i > (1024 * 1024 * 1024):
+		eFmt = "G"
+		n = float64(i) / 1024 / 1024 / 1024
+	case i > (1024 * 1024):
+		eFmt = "M"
+		n = float64(i) / 1024 / 1024
+	case i > 1024:
+		eFmt = "K"
+		n = float64(i) / 1024
+	default:
+		sFmt = "%.0f"
+		n = float64(i)
+	}
+	if eFmt != "" && n >= 10 {
+		sFmt = "%.0f"
+	}
+	result = fmt.Sprintf(sFmt+eFmt, n)
+	result = strings.Trim(result, " ")
+	return
 }
