e9bd76c6199ae8f4449e1ad0e3f6de07973e0597
Ariel Mashraki <ariel@mashraki.co.il>
2015-12-11 22:08:27 +0200

    add color

---
+++ node/color.go
@@ -0,0 +1,44 @@
+package node
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
+const Escape = "\x1b"
+const (
+	Reset int = 0
+	Bold  int = 1
+	Black int = iota + 28
+	Red
+	Green
+	Yello
+	Blue
+	Magenta
+	Cyan
+	White
+)
+
+func ansiFormat(color int, s string) string {
+	return fmt.Sprintf("%s[%d;%dm%s%s[%dm", Escape, Bold, color, s, Escape, Reset)
+}
+
+// ANSIColor
+func ANSIColor(node *Node, s string) string {
+	var color int
+	switch ext := filepath.Ext(node.Name()); strings.ToLower(ext) {
+	case ".bat", ".btm", ".cmd", ".com", ".dll", ".exe":
+		color = Green
+	case ".arj", ".bz2", ".deb", ".gz", ".lzh", ".rpm", ".tar", ".taz", ".tb2", ".tbz2",
+		".tbz", ".tgz", ".tz", ".tz2", ".z", ".zip", ".zoo":
+		color = Red
+	case ".asf", ".avi", ".bmp", ".flac", ".gif", ".jpg", "jpeg", ".m2a", ".m2v", ".mov",
+		".mp3", ".mpeg", ".mpg", ".ogg", ".ppm", ".rm", ".tga", ".tif", ".wav", ".wmv",
+		".xbm", ".xpm":
+		color = Magenta
+	}
+	return ansiFormat(color, s)
+}
+
+// TODO: HTMLColor
