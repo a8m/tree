a8m/tree

commit ce3525c5c2ef08dc68534c35c6897323a70c3c08

Add prune and matchdirs options (#18)
* add `--prune` option with tests

* add partial `--matchdirs`, one test still failing

* add `--matchdirs` option

* also adjust behavior for `IPattern` + `MatchDirs`, partial, 2 failed tests

* fixed two failing test cases for `IPattern` + `MatchDirs`

* Remove unused print statement

Co-authored-by: Ariel Mashraki <7413593+a8m@users.noreply.github.com>

* accept suggested change to `dirMatch` bool

* add suggested style changes

* apply suggested change to group conditions under `nnode.IsDir()`

Co-authored-by: Ariel Mashraki <7413593+a8m@users.noreply.github.com>

node.go
@@ -49,6 +49,8 @@ type Options struct {
 	DeepLevel  int
 	Pattern    string
 	IPattern   string
+	MatchDirs  bool
+	Prune      bool
 	// File
 	ByteSize bool
 	UnitSize bool
@@ -113,6 +115,16 @@ func (node *Node) Visit(opts *Options) (dirs, files int) {
 	if opts.DeepLevel > 0 && opts.DeepLevel <= node.depth {
 		return
 	}
+	// MatchDirs option
+	var dirMatch bool
+	if node.depth != 0 && opts.MatchDirs {
+		// then disable prune and pattern for immediate children
+		if opts.Pattern != "" {
+			dirMatch = node.match(opts.Pattern, opts)
+		} else if opts.IPattern != "" && node.match(opts.IPattern, opts) {
+			return
+		}
+	}
 	names, err := opts.Fs.ReadDir(node.path)
 	if err != nil {
 		node.err = err
@@ -130,28 +142,26 @@ func (node *Node) Visit(opts *Options) (dirs, files int) {
 			vpaths: node.vpaths,
 		}
 		d, f := nnode.Visit(opts)
-		if nnode.err == nil && !nnode.IsDir() {
+		if nnode.IsDir() {
+			// "prune" option, hide empty directories
+			if opts.Prune && f == 0 {
+				continue
+			}
+			if opts.MatchDirs && opts.IPattern != "" && nnode.match(opts.IPattern, opts) {
+				continue
+			}
+		} else if nnode.err == nil {
 			// "dirs only" option
 			if opts.DirsOnly {
 				continue
 			}
-			var rePrefix string
-			if opts.IgnoreCase {
-				rePrefix = "(?i)"
-			}
 			// Pattern matching
-			if opts.Pattern != "" {
-				re, err := regexp.Compile(rePrefix + opts.Pattern)
-				if err == nil && !re.MatchString(name) {
-					continue
-				}
+			if !dirMatch && opts.Pattern != "" && !nnode.match(opts.Pattern, opts) {
+				continue
 			}
 			// IPattern matching
-			if opts.IPattern != "" {
-				re, err := regexp.Compile(rePrefix + opts.IPattern)
-				if err == nil && re.MatchString(name) {
-					continue
-				}
+			if opts.IPattern != "" && nnode.match(opts.IPattern, opts) {
+				continue
 			}
 		}
 		node.nodes = append(node.nodes, nnode)
@@ -162,6 +172,19 @@ func (node *Node) Visit(opts *Options) (dirs, files int) {
 		node.sort(opts)
 	}
 	return
+}
+
+func (node *Node) match(pattern string, opt *Options) bool {
+	var prefix string
+	if opt.IgnoreCase {
+		prefix = "(?i)"
+	}
+	search := node.Name()
+	if strings.Contains(pattern, "*") {
+		search = node.path
+	}
+	re, err := regexp.Compile(prefix + pattern)
+	return err == nil && re.FindString(search) != ""
 }
 
 func (node *Node) sort(opts *Options) {

node_test.go
@@ -109,47 +109,133 @@ var listTests = []treeTest{
 	{"basic", &Options{Fs: fs, OutFile: out}, `root
 ├── a
 ├── b
-└── c
-    ├── d
-    └── e
-`, 1, 4},
+├── c
+│   ├── d
+│   ├── e
+│   ├── g
+│   │   ├── h
+│   │   └── i
+│   └── k
+└── j
+`, 2, 8},
 	{"all", &Options{Fs: fs, OutFile: out, All: true, NoSort: true}, `root
 ├── a
 ├── b
-└── c
-    ├── d
-    ├── e
-    └── .f
-`, 1, 5},
+├── c
+│   ├── d
+│   ├── e
+│   ├── .f
+│   ├── g
+│   │   ├── h
+│   │   └── i
+│   └── k
+└── j
+`, 2, 9},
 	{"dirs", &Options{Fs: fs, OutFile: out, DirsOnly: true}, `root
 └── c
-`, 1, 0},
+    └── g
+`, 2, 0},
 	{"fullPath", &Options{Fs: fs, OutFile: out, FullPath: true}, `root
 ├── root/a
 ├── root/b
-└── root/c
-    ├── root/c/d
-    └── root/c/e
-`, 1, 4},
+├── root/c
+│   ├── root/c/d
+│   ├── root/c/e
+│   ├── root/c/g
+│   │   ├── root/c/g/h
+│   │   └── root/c/g/i
+│   └── root/c/k
+└── root/j
+`, 2, 8},
 	{"deepLevel", &Options{Fs: fs, OutFile: out, DeepLevel: 1}, `root
 ├── a
 ├── b
-└── c
-`, 1, 2},
-	{"pattern", &Options{Fs: fs, OutFile: out, Pattern: "(a|e)"}, `root
+├── c
+└── j
+`, 1, 3},
+	{"pattern (a|e|i)", &Options{Fs: fs, OutFile: out, Pattern: "(a|e|i)"}, `root
 ├── a
 └── c
+    ├── e
+    └── g
+        └── i
+`, 2, 3},
+	{"pattern (x) + 0 files", &Options{Fs: fs, OutFile: out, Pattern: "(x)"}, `root
+└── c
+    └── g
+`, 2, 0},
+	{"ipattern (a|e|i)", &Options{Fs: fs, OutFile: out, IPattern: "(a|e|i)"}, `root
+├── b
+├── c
+│   ├── d
+│   ├── g
+│   │   └── h
+│   └── k
+└── j
+`, 2, 5},
+	{"pattern (A) + ignore-case", &Options{Fs: fs, OutFile: out, Pattern: "(A)", IgnoreCase: true}, `root
+├── a
+└── c
+    └── g
+`, 2, 1},
+	{"pattern (A) + ignore-case + prune", &Options{Fs: fs, OutFile: out, Pattern: "(A)", Prune: true, IgnoreCase: true}, `root
+└── a
+`, 0, 1},
+	{"pattern (a) + prune", &Options{Fs: fs, OutFile: out, Pattern: "(a)", Prune: true}, `root
+└── a
+`, 0, 1},
+	{"pattern (c) + matchdirs", &Options{Fs: fs, OutFile: out, Pattern: "(c)", MatchDirs: true}, `root
+└── c
+    ├── d
+    ├── e
+    ├── g
+    └── k
+`, 2, 3},
+	{"pattern (c.*) + matchdirs", &Options{Fs: fs, OutFile: out, Pattern: "(c.*)", MatchDirs: true}, `root
+└── c
+    ├── d
+    ├── e
+    ├── g
+    │   ├── h
+    │   └── i
+    └── k
+`, 2, 5},
+	{"ipattern (c) + matchdirs", &Options{Fs: fs, OutFile: out, IPattern: "(c)", MatchDirs: true}, `root
+├── a
+├── b
+└── j
+`, 0, 3},
+	{"ipattern (g) + matchdirs", &Options{Fs: fs, OutFile: out, IPattern: "(g)", MatchDirs: true}, `root
+├── a
+├── b
+├── c
+│   ├── d
+│   ├── e
+│   └── k
+└── j
+`, 1, 6},
+	{"ipattern (a|e|i|h) + matchdirs + prune", &Options{Fs: fs, OutFile: out, IPattern: "(a|e|i|h)", MatchDirs: true, Prune: true}, `root
+├── b
+├── c
+│   ├── d
+│   └── k
+└── j
+`, 1, 4},
+	{"pattern (d|e) + prune", &Options{Fs: fs, OutFile: out, Pattern: "(d|e)", Prune: true}, `root
+└── c
+    ├── d
     └── e
 `, 1, 2},
-	{"ipattern", &Options{Fs: fs, OutFile: out, IPattern: "(a|e)"}, `root
-├── b
+	{"pattern (c.*) + matchdirs + prune ", &Options{Fs: fs, OutFile: out, Pattern: "(c.*)", Prune: true, MatchDirs: true}, `root
 └── c
-    └── d
-`, 1, 2},
-	{"ignore-case", &Options{Fs: fs, OutFile: out, Pattern: "(A)", IgnoreCase: true}, `root
-├── a
-└── c
-`, 1, 1}}
+    ├── d
+    ├── e
+    ├── g
+    │   ├── h
+    │   └── i
+    └── k
+`, 2, 5},
+}
 
 func TestSimple(t *testing.T) {
 	root := &file{
@@ -165,8 +251,18 @@ func TestSimple(t *testing.T) {
 					{name: "d", size: 50},
 					{name: "e", size: 50},
 					{name: ".f", size: 0},
+					{
+						name: "g",
+						size: 100,
+						files: []*file{
+							{name: "h", size: 50},
+							{name: "i", size: 50},
+						},
+					},
+					{name: "k", size: 50},
 				},
 			},
+			{name: "j", size: 50},
 		},
 	}
 	fs.clean().addFile(root.name, root)
@@ -177,7 +273,7 @@ func TestSimple(t *testing.T) {
 			t.Errorf("wrong dir count for test %q:\ngot:\n%d\nexpected:\n%d", test.name, d, test.dirs)
 		}
 		if f != test.files {
-			t.Errorf("wrong dir count for test %q:\ngot:\n%d\nexpected:\n%d", test.name, d, test.files)
+			t.Errorf("wrong file count for test %q:\ngot:\n%d\nexpected:\n%d", test.name, f, test.files)
 		}
 		inf.Print(test.opts)
 		if !out.equal(test.expected) {

commit 4eeea41fc846afb9845ced4952d69e40bff47a84

Fix nil pointer exception in Visit on stat error
Before this change, if an error occurred when stat-ing a file, it
could cause a nil-pointer exception error in Visit.

This error was introduced in

    ce3525c Add prune and matchdirs options (#18)

Which refactored the error check into the wrong place.

See: https://forum.rclone.org/t/error-with-build-v1-61-1-tree-command-panic-runtime-error-invalid-memory-address-or-nil-pointer-dereference/35922/

node.go
@@ -142,26 +142,28 @@ func (node *Node) Visit(opts *Options) (dirs, files int) {
 			vpaths: node.vpaths,
 		}
 		d, f := nnode.Visit(opts)
-		if nnode.IsDir() {
-			// "prune" option, hide empty directories
-			if opts.Prune && f == 0 {
-				continue
-			}
-			if opts.MatchDirs && opts.IPattern != "" && nnode.match(opts.IPattern, opts) {
-				continue
-			}
-		} else if nnode.err == nil {
-			// "dirs only" option
-			if opts.DirsOnly {
-				continue
-			}
-			// Pattern matching
-			if !dirMatch && opts.Pattern != "" && !nnode.match(opts.Pattern, opts) {
-				continue
-			}
-			// IPattern matching
-			if opts.IPattern != "" && nnode.match(opts.IPattern, opts) {
-				continue
+		if nnode.err == nil {
+			if nnode.IsDir() {
+				// "prune" option, hide empty directories
+				if opts.Prune && f == 0 {
+					continue
+				}
+				if opts.MatchDirs && opts.IPattern != "" && nnode.match(opts.IPattern, opts) {
+					continue
+				}
+			} else {
+				// "dirs only" option
+				if opts.DirsOnly {
+					continue
+				}
+				// Pattern matching
+				if !dirMatch && opts.Pattern != "" && !nnode.match(opts.Pattern, opts) {
+					continue
+				}
+				// IPattern matching
+				if opts.IPattern != "" && nnode.match(opts.IPattern, opts) {
+					continue
+				}
 			}
 		}
 		node.nodes = append(node.nodes, nnode)